2d15ea5d2f73ee3ab4d9e1e29770d58b266707e5
Blake Reece <frankiereece3@gmail.com>
2023-01-30 20:13:33 -0700

    added date

--- script.js
+++ script.js
@@ -20,4 +20,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var date = dayjs().format('DD/MM/YYYY')
+  console.log(date)
+  $("#currentDay").text(date)
 });
